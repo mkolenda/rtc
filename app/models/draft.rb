a1ef7433ba2e71c90cff4fcb243961fba6360e4e
Justin Ellis <justinellis110@gmail.com>
2019-11-11 03:21:48 -0500

    adds callbacks to draft

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -2,4 +2,22 @@ class Draft < ActiveRecord::Base
   belongs_to :proposal
   belongs_to :author
   has_many :external_links
+
+  delegate :author, to: :proposal
+
+  before_validation :set_version, :set_written_at, :set_state
+
+  private
+
+  def set_version
+    self.version ||= 1
+  end
+
+  def set_written_at
+    self.written_at = Time.now
+  end
+
+  def set_state
+    self.state ||= 'New'
+  end
 end
