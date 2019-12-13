80cc47f6a155b1b06d9d4e603b1957f5cc605239
Aaron Kelton <aaron.k.kelton@gmail.com>
2019-12-13 13:50:45 -0600

    whitelist params instead of allowing mass-assignment

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,7 +1,5 @@
 class ProposalsController < ApplicationController
   def create
-    params.require(:proposal).permit!
-    params.require(:draft).permit!
     @author_id = params[:proposal][:author_id]
     @proposal = Proposal.create(params[:proposal])
     @proposal.author_id = @author_id
@@ -24,5 +22,20 @@ class ProposalsController < ApplicationController
   def show
     @proposal = Proposal.find(params[:id])
   end
+
+  private
+    # I stole these comments from https://guides.rubyonrails.org/v4.2/action_controller_overview.html#strong-parameters
+    # ---
+    # Using a private method to encapsulate the permissible parameters
+    # is just a good pattern since you'll be able to reuse the same
+    # permit list between create and update. Also, you can specialize
+    # this method with per-user checking of permissible attributes.
+    def proposal_params
+      params.require(:proposal).permit(:title, :body, :reviewed)
+    end
+
+    def draft_params
+      params.require(:draft).permit(:version, :state)
+    end
 
 end
